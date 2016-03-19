7a69424b5c8f1c167d786f9dc98ef4b771fb02e4
Caleb Jones <self@calebjones.net>
2016-03-19 16:00:03 -0400

    Add color transitions

--- website/main.js
+++ website/main.js
@@ -1,3 +1,19 @@
+function lerp(initial, final, factor) {
+    return initial + (final - initial) * factor;
+}
+
+function lerpColor(initial, final, factor) {
+    return [
+        lerp(initial[0], final[0], factor),
+        lerp(initial[1], final[1], factor),
+        lerp(initial[2], final[2], factor),
+    ];
+}
+
+function toCssColor(r, g, b) {
+    return 'rgb(' + (r|0) + ', ' + (g|0) + ', ' + (b|0) + ')';
+}
+
 scrollProgress = 0;
 function updateScrollProgress(delta) {
     scrollProgress += delta;
@@ -6,16 +22,18 @@ function updateScrollProgress(delta) {
     }
 }
 
+colors = [[255, 255, 255], [255, 255, 0], [0, 255, 0], [0, 255, 255], [0, 0, 255], [255, 0, 255], [255, 0, 0]];
+
 function onScroll(delta) {
     updateScrollProgress(delta.deltaY);
-    var color = '#FFF';
-    if (scrollProgress > 120) {
-        var color = (scrollProgress - 120) / 100;
-        color = (255 - Math.min(color, 1)*255)|0;
-        color = 'rgb(' + color + ', ' + color + ', ' + color + ')';
+    var region = Math.floor(scrollProgress/150);
+    var regionProgress = (scrollProgress % 150)/150;
+    if (regionProgress > 0.66) {
+        var transitionProgress = (regionProgress - 0.66) * 3;
+        var color = lerpColor(colors[region], colors[region+1], transitionProgress);
+        color = toCssColor.apply(null, color);
+        document.body.style.backgroundColor = color;
     }
-    document.body.style.backgroundColor = color;
-    console.log(color);
 }
 
 document.addEventListener('wheel', onScroll);
